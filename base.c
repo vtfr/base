7c806c93ec070823c57208bdc1e675ad740bef8f
Victor Franco <victorfrancovl@gmail.com>
2014-11-08 11:04:41 -0300

    Correção do vetor de representações

--- base.c
+++ base.c
@@ -7,7 +7,7 @@ char repr[] = {
 	'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
 	'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
 	'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
-	'U', 'W', 'X', 'Y', 'Z'
+	'U', 'V', 'W', 'X', 'Y', 'Z'
 };
 
 /* Representa um número em uma base específica */
